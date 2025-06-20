95ce7160b75b69cc7157374c928b8d0c8458e918
hushuhan <mechwho@qq.com>
2025-06-20 12:55:51 +0800

    feat(ball): 添加小球移动和遇到边缘反弹的逻辑

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -18,7 +18,15 @@ public class Game : MonoBehaviour
 
     private int _leftScore = 0, _rightScore = 0; // 比分
     
+    // 挡板
     private float _leftMoveSpeed = 0, _rightMoveSpeed = 0; // 挡板移动速度 
+    
+    // 单位与像素坐标比例为1：100
+    private float _pixelScale = 100.0f;
+
+    //小球移动速度和方向
+    private float _ballSpeed = 10.0f;
+    private float _ballSpeedAngle = 0.25f * Mathf.PI;  //初始角度为pi/4
     
     // Start is called before the first frame update
     void Start()
@@ -37,6 +45,54 @@ public class Game : MonoBehaviour
     {
         ControlPanel(ref _leftBlock, ref _leftMoveSpeed, KeyCode.W, KeyCode.S);
         ControlPanel(ref _rightBlock, ref _rightMoveSpeed, KeyCode.UpArrow, KeyCode.DownArrow);
+        CalculateBallMove();
+        CalculateBallCollision();
+    }
+
+    private void CalculateBallMove()
+    {
+        //计算小球移动速度——极坐标
+        float speedY = Mathf.Cos(_ballSpeedAngle) * _ballSpeed/_pixelScale;
+        float speedX = Mathf.Sin(_ballSpeedAngle) * _ballSpeed/_pixelScale;
+
+        //小球移动
+        Vector2 ballPosition = _ball.transform.position;
+        ballPosition.x += speedX;
+        ballPosition.y += speedY;
+        _ball.transform.position = ballPosition;
+    }
+
+    private void CalculateBallCollision()
+    {
+        Vector2 ballPosition = _ball.transform.position;
+        //小球碰撞到左右边缘后反弹
+        if (ballPosition.x > 6.6f )
+        { // 右侧
+            _ballSpeedAngle = -_ballSpeedAngle;
+            _leftScore++;
+            leftScoreText.text = _leftScore.ToString();
+        } else if (ballPosition.x < -6.6f )
+        { // 左侧
+            _ballSpeedAngle = -_ballSpeedAngle;
+            _rightScore++;
+            rightScoreText.text = _rightScore.ToString();
+        }
+
+        if (ballPosition.y > 5.0f)
+        { // 上方
+            // 反弹
+            _ballSpeedAngle = Mathf.PI - _ballSpeedAngle;
+            // 上下联通
+            // ballPosition.y = -5.0f;
+            // _ball.transform.position = ballPosition;
+        } else if (ballPosition.y < -5.0f)
+        { // 下方
+            // 反弹
+            _ballSpeedAngle = Mathf.PI - _ballSpeedAngle;
+            // 上下联通
+            // ballPosition.y = 5.0f;
+            // _ball.transform.position = ballPosition;
+        }
     }
 
     private void ControlPanel(ref GameObject block, ref float moveSpeed, KeyCode upKeyCode, KeyCode downKeyCode)
